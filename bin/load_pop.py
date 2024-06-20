8430e3e05d0c6817ebe1e0f427af12c8714c9fd1
Elizabeth Teng <eteng@protonmail.com>
2024-06-20 07:27:42 -0500

    modified output data included in df

--- bin/load_pop.py
+++ bin/load_pop.py
@@ -19,12 +19,11 @@ p = pop.oneline["orbital_period_i"]
 fstate1 = pop.oneline["S1_state_f"]
 fstate2 = pop.oneline["S2_state_f"]
 
-bbh = np.logical_and(fstate1=="BH",fstate2=="BH")
-
 data = {"m1" : m1,
           "m2" : m2,
           "p" : p,
-          "BBH" : bbh
+          "fstate1" : fstate1,
+          "fstate2" : fstate2
          }
 
 df = pd.DataFrame(data)
