f1cd1f24725c66d9b9dd55eaceecb1b9483b4163
Elizabeth Teng <eteng@protonmail.com>
2024-06-20 05:23:01 -0500

    fixing one more time lol

--- load_pop.py
+++ load_pop.py
@@ -11,7 +11,7 @@ p = pop.oneline["orbital_period_i"]
 fstate1 = pop.oneline["S1_state_f"]
 fstate2 = pop.oneline["S2_state_f"]
 
-bbh = np.logical_and(fstate1=="BBH",fstate2=="BBH")
+bbh = np.logical_and(fstate1=="BH",fstate2=="BH")
 
 data = {"m1" : m1,
           "m2" : m2,
