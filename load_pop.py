d9f6ae2b5bd7127824e7b83b76975d1b6586df70
Elizabeth Teng <eteng@protonmail.com>
2024-06-20 05:22:42 -0500

    fixing bbh condition

--- load_pop.py
+++ load_pop.py
@@ -11,7 +11,7 @@ p = pop.oneline["orbital_period_i"]
 fstate1 = pop.oneline["S1_state_f"]
 fstate2 = pop.oneline["S2_state_f"]
 
-bbh = np.logical_and(fstate1==fstate2 ,fstate1=="BBH")
+bbh = np.logical_and(fstate1=="BBH",fstate2=="BBH")
 
 data = {"m1" : m1,
           "m2" : m2,
